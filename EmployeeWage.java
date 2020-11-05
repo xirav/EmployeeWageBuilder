df9e9efa5e78fbeaf017be1f03788836e05d8967
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-05 16:48:24 +0530

    ADD UC6 employee wage for 100 hours is reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,18 +5,23 @@ public class EmployeeWage {
                 public static final int IS_FULL_TIME = 2;
                 public static final int EMP_RATE_PER_HOUR = 20;
 		public static final int NUM_OF_WORKING_DAYS = 20;
+		public static final int TOTAL_WORKING_HOURS = 100;
 
 	public static void main(String[] args) {
 
 		//Variables
 
-		int empHrs=0;
-		int empWage=0;
+		
+		
 		int totalEmpWage=0;
+		int totalEmpHrs=0;
+		int totalWorkingDays=0;
 
 		//computations
 		
-		for (int day = 0 ; day < NUM_OF_WORKING_DAYS ; day ++ ) {
+		while (totalEmpHrs <= TOTAL_WORKING_HOURS && totalWorkingDays < NUM_OF_WORKING_DAYS ){
+			int empHrs = 0;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
                 		 case IS_FULL_TIME:
@@ -34,8 +39,8 @@ public class EmployeeWage {
 				
 
 			}
-
-			empWage=empHrs * EMP_RATE_PER_HOUR;
+			totalEmpHrs += empHrs;
+			int empWage=empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage += empWage;
 			System.out.println("EmpWage: " + empWage);
 		}
