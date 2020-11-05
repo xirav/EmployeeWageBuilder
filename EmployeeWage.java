0e89e4308ba77873c924407cd7666d399ccfedaa
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-05 16:31:24 +0530

    ADD UC5 employee monthly wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
                 public static final int IS_PART_TIME = 1;
                 public static final int IS_FULL_TIME = 2;
                 public static final int EMP_RATE_PER_HOUR = 20;
+		public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 
@@ -11,28 +12,36 @@ public class EmployeeWage {
 
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
 
 		//computations
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		switch ((int)empCheck) {
-                	 case IS_FULL_TIME:
+		
+		for (int day = 0 ; day < NUM_OF_WORKING_DAYS ; day ++ ) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+                		 case IS_FULL_TIME:
+				
+			 		empHrs=8;
+					break;
 
-			 	empHrs=8;
-				break;
+		 		case IS_PART_TIME:
 
-		 	case IS_PART_TIME:
+					empHrs = 4;
+					break;
 
-				empHrs = 4;
-				break;
-
-			default:
-				empHrs = 0;
+				default:
+					empHrs = 0;
 				
 
-		}
+			}
 
-		empWage=empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("EmpWage: " + empWage);
+			empWage=empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("EmpWage: " + empWage);
+		}
+		System.out.println("totalWage: " + totalEmpWage);
+
+	
 
 	}
 
