1716c454c16b0b678d18a909fbd2a231a9f40b8e
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-05 15:48:30 +0530

    UC3 EMployee Wage PartTime

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,19 +2,20 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		//constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME =1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		//Variables
 		int empHrs=0;
 		int empWage=0;
 		//computations
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
 			empHrs=8;
-			empWage=empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("EmpWage: " + empWage);
-		}
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 4;
+		empWage=empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("EmpWage: " + empWage);
 
 	}
 
