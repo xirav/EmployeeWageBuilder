d34dc63a94750b43f9482b726e0c6e835755e646
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-05 15:31:49 +0530

    ADD UC2 Employee Wage computations

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,13 +3,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		//constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
 		//computations
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == IS_FULL_TIME)
+		if(empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Abscent");
-
+			empHrs=8;
+			empWage=empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("EmpWage: " + empWage);
+		}
 
 	}
 
