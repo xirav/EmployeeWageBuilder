043c5ee1946aa680d2b8d792cb9b2ce4f0b4e859
JaiVenkateshwaraTechnologies <kuldeephadnoorkat@gmail.com>
2020-11-05 16:13:25 +0530

    ADD UC4 Employee Switch Case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,36 @@
 public class EmployeeWage {
 
+	  //constants
+                public static final int IS_PART_TIME = 1;
+                public static final int IS_FULL_TIME = 2;
+                public static final int EMP_RATE_PER_HOUR = 20;
+
 	public static void main(String[] args) {
-		//constants
-		int IS_PART_TIME =1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+
 		//Variables
+
 		int empHrs=0;
 		int empWage=0;
+
 		//computations
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs = 4;
+		switch ((int)empCheck) {
+                	 case IS_FULL_TIME:
+
+			 	empHrs=8;
+				break;
+
+		 	case IS_PART_TIME:
+
+				empHrs = 4;
+				break;
+
+			default:
+				empHrs = 0;
+				
+
+		}
+
 		empWage=empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("EmpWage: " + empWage);
 
